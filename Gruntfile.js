dba54d0128ac2fb675759527cb43768f941c5336
Pierre Romera <hello@pirhoo.com>
2014-02-02 23:24:25 +0100

    Remove img and font suffix

--- Gruntfile.js
+++ Gruntfile.js
@@ -320,9 +320,7 @@ module.exports = function (grunt) {
         files: [{
           src: [
             '<%= yeoman.dist %>/js/**/*.js',
-            '<%= yeoman.dist %>/css/**/*.css',
-            '<%= yeoman.dist %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}',
-            '<%= yeoman.dist %>/fonts/**/*.{eot*,otf,svg,ttf,woff}'
+            '<%= yeoman.dist %>/css/**/*.css'
           ]
         }]
       }
@@ -362,8 +360,7 @@ module.exports = function (grunt) {
       },
       check: {
         src: [
-          '<%= yeoman.app %>/css/**/*.css',
-          '<%= yeoman.app %>/_scss/**/*.css'
+          '<%= yeoman.app %>/css/**/*.css'
         ]
       }
     },
